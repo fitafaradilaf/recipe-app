efac13782417367ae4e43a84cafa5b7844aa60d0
fitafaradilaf <84325453+fitafaradilaf@users.noreply.github.com>
2024-05-20 18:52:00 +0300

    Create category_list.dart

---
+++ lib/constans/category_list.dart
@@ -0,0 +1,42 @@
+import 'package:recipeapps/constans/images_path.dart';
+
+List<String> name=[
+  'Breakfast',
+  'Lunch',
+  'Dinner',
+  'Supper',
+
+];
+
+List<String> images=[
+  ImagesPath.breakfast,
+  ImagesPath.lunch,
+  ImagesPath.dinner,
+  ImagesPath.supper,
+];
+
+List categories=[
+  'chocolate',
+  'drink',
+  'fast food',
+  'fish',
+  'ice cream',
+  'juice',
+  'pop corn',
+  'tasty',
+  'vegetable',
+  'sweets',
+];
+
+List categoryImage=[
+  ImagesPath.chocolate,
+  ImagesPath.drink,
+  ImagesPath.fastfood,
+  ImagesPath.fish,
+  ImagesPath.iceCream,
+  ImagesPath.juice,
+  ImagesPath.pop,
+  ImagesPath.tasty,
+  ImagesPath.vegetable,
+  ImagesPath.tasty,
+];
